3cee59fc9b2316529505d2234f630596c081c7e5
arpit-s02 <arpitrahul02@gmail.com>
2023-02-07 00:52:59 +0530

    create card component and render the data received from the API

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,16 +1,26 @@
-const Card = (props) => {
+import styles from "../Styles/card.module.css";
+
+const Card = ({ item }) => {
+  const label = item.recipe.label;
+
+  const nameArray = label.split(" ", 2);
+  let itemName = nameArray[0] + " ";
+  if (nameArray[1] !== undefined) {
+    itemName += nameArray[1];
+  }
+
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <a href="props.url">
-          <h2></h2>
-        </a>
-        <a href="/">
-          <button>See More...</button>
-        </a>
+    <div className={styles.allCardsContainer}>
+      <div className={styles.cardContainer}>
+        <div className={styles.recipeImage}>
+          <img src={item.recipe.image} alt="" />
+        </div>
+        <div className={styles.recipeInfo}>
+          <span> {itemName} </span>
+          <a href="/">
+            <button style={{ cursor: "pointer" }}> See More </button>
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,30 +1,56 @@
 import React, { useState } from "react";
+import Card from "../Components/Card";
+import styles from "../Styles/home.module.css";
 
 const Home = () => {
   const [input, setInput] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const APP_ID = "ba520093";
   const APP_KEY = "4573bab7197f249f9caa698160a81391";
 
-  const fetchRecipes = async () => {
+  const fetchRecipes = async (e) => {
+    setLoading(true);
+    e.preventDefault();
     let response = await fetch(
       `https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
-    console.log(data);
+    console.log(data.hits);
+    setRecipes(data.hits);
+    setLoading(false);
   };
 
   return (
-    <div>
-      <h1>Recipe Search</h1>
-      <input
-        type="search"
-        placeholder="Search a recipe"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={fetchRecipes}> Search </button>
-      <div>{/* <Card /> */}</div>
+    <div className={styles.homeContainer}>
+      <div className={styles.search}>
+        <h1>Recipe Search</h1>
+        <div>
+          <form onSubmit={fetchRecipes}>
+            <input
+              className={styles.searchBar}
+              type="search"
+              placeholder="Search a recipe"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={fetchRecipes}> Search </button>
+          </form>
+        </div>
+      </div>
+
+      {loading ? (
+        <div> Loading... </div>
+      ) : (
+        recipes.length > 0 && (
+          <div className={styles.allCardsContainer}>
+            {recipes.map((item, index) => (
+              <Card item={item} key={index} />
+            ))}
+          </div>
+        )
+      )}
     </div>
   );
 };
